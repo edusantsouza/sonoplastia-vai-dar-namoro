acb1bea893cd6cd8f3dfc2d3671e4c3d771dda0c
edusantsouza <eduardo.santsouza@gmail.com>
2022-07-04 21:54:33 -0300

    Primeiro commit. Ele goooosta.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return 
+console.log(audio)
+  audio.currentTime = 0
+  audio.play()
+
+  key.classList.add('playing')
+})
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition)
+})
